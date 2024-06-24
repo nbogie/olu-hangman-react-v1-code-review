f061c0b229cee97dbce7a2db95f8f4374d001ac9
OluC94 <coker.olu94@gmail.com>
2024-06-24 10:29:25 +0100

    render keyboard

--- src/Hangman.jsx
+++ src/Hangman.jsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
-import KeyboardReact from "react-simple-keyboard";
-import 'react-simple-keyboard/build/css/index.css'
 import getRandomWord from "./utils/get-random-word";
+import generateKeys from "./utils/keyboard";
 
 export default function Hangman() {
-    const [guessedLetters, setGuessedLetters] = useState()
-    const [targetWord, setTargetWord] = useState(getRandomWord());
-    const [numberOfGuesses, setNumberOfGuesses] = useState(0);
+    const [guessedLetters, setGuessedLetters] = useState([]) // string[]
+    const [targetWord, setTargetWord] = useState(getRandomWord()); // string
+    const [numberOfGuesses, setNumberOfGuesses] = useState(0); // int
+    const keys = generateKeys();
+    const renderedKeyboard = keys.map((tile, index) => {
+        return (
+            <button key={index} className="tile" >
+                {tile}
+            </button>
+        )
+    })
 
-    function handleKeyPress(button) {
-        console.log("button pressed: ", button)
-    }
     return (
         <div>
             <h1>Hangman Game</h1>
             <h2>Output area</h2>
             <h2>Number of guesses</h2>
-            <KeyboardReact onKeyPress={handleKeyPress} />
+            <div className="keyboard">
+                {renderedKeyboard}
+            </div>
         </div>
     );
 }
---
+++ src/utils/keyboard.js
@@ -0,0 +1,4 @@
+export default function generateKeys(){
+    const qwerty = "qwertyuiopasdfghjklzxcvbnm"
+    return qwerty.split('');
+}
